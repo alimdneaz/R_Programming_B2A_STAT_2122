7da55cd5aeb78531b9e08a1224ffdffddb4f4509
Ali Neaz <neazali77@gmail.com>
2025-05-09 21:47:31 +0600

    add first day

---
+++ 01_day_01/01_first_file.R
@@ -0,0 +1,65 @@
+
+
+
+# install packages
+install.packages("dplyr")
+library(dplyr)
+
+# help
+help("mean")
+?mean
+
+
+
+
+# Checking working directory
+getwd()
+
+
+# D:/041_STAT_2122/01_day_01
+
+setwd("D:/041_STAT_2122/01_day_01")
+
+
+# Reminder Operator
+
+23 %% 5
+# Problem: Identify if a number if odd or even.
+21%%2
+20%%2
+
+
+# Logical Operator
+TRUE & TRUE
+TRUE & FALSE
+
+TRUE | FALSE
+!FALSE
+
+
+age = 45
+name <- "Kisu Ekta"
+
+age
+name
+
+
+# %in%
+# %*%
+
+# Variables
+
+rsults <- rnorm(100, 75, 5) %>% round(1)
+rsults
+
+rna.seq_data = 5
+# rna.seq_data2
+# rnaseqdata
+
+#~!@#$%^&*
+
+# for 
+# while
+# repeat
+# TRUE
+# FALSE
